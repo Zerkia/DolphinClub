68a11241369801a4e109402a7ce7186e4cbf5516
Mads Pontoppidan Haderup <mads@haderup.net>
2021-05-19 14:11:37 +0200

    Sort by PersonalBest improved/corrected

--- src/PersonalBestSorter.java
+++ src/PersonalBestSorter.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Comparator;
 
 public class PersonalBestSorter implements Comparator<KonkurrenceSvømmer> {
@@ -5,7 +6,13 @@ public class PersonalBestSorter implements Comparator<KonkurrenceSvømmer> {
 
     @Override
     public int compare(KonkurrenceSvømmer o1, KonkurrenceSvømmer o2) {
-        return Integer.parseInt(o1.getPersonalBest().replace(":","")) - Integer.parseInt(o2.getPersonalBest().replace(":",""));
+        String [] i = (o1.getPersonalBest().split(":"));
+        int j = Integer.parseInt(i[0])*60+Integer.parseInt(i[1]);
+        String [] k = (o2.getPersonalBest().split(":"));
+        int l = Integer.parseInt(k[0])*60+Integer.parseInt(k[1]);
+
+        return j - l;
+        //return Integer.parseInt(o1.getPersonalBest().replace(":","")) - Integer.parseInt(o2.getPersonalBest().replace(":",""));
 
     }
 }
